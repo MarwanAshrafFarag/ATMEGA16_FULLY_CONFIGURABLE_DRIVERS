05dfabbcfda842a642c5a7f16ca5974a9e59e75d
MarwanAshrafFarag <marwan.ashraf.farag@gmail.com>
2022-06-07 01:50:52 +0200

    Created CPU Configuration File CPU.h
    This file holds the configuration of the controller as well as the includes of all the modules that will be used in the project.

---
+++ CPU.h
@@ -0,0 +1,22 @@
+/*
+ * CPU.h
+ *
+ *  Created on: Jun 6, 2022
+ *      Author: bulls
+ */
+
+#ifndef CPU_H_
+#define CPU_H_
+
+#include <avr/io.h>		/* Include AVR std. library file */
+#include <avr/interrupt.h>
+#include <util/delay.h>	/* Include Delay header file */
+
+#ifdef F_CPU
+#undef F_CPU
+#define F_CPU 16000000UL
+//#define __OPTIMIZE__
+
+#endif /* F_CPU */
+
+#endif /* CPU_H_ */
