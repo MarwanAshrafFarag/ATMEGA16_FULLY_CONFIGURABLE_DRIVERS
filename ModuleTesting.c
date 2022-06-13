5679796fe3aa0afff4039e6dda46c1e8a705abe4
MarwanAshrafFarag <marwan.ashraf.farag@gmail.com>
2022-06-13 03:11:14 +0200

    Set the FOC0A pin in timer control register for optimized performance, Timer overflow mode completely tested with polling

--- ModuleTesting.c
+++ ModuleTesting.c
@@ -3,27 +3,36 @@
 #include "Timer.h"
 uint8 counter;
 
-/*ISR (TIMER0_OVF_vect)
-{
-	counter++;
-}*/
+
+/****************************************
+ * 										*
+ *  OVERFLOW MODE, INTERRUPT DISABLED	*
+ *                                      *
+ ***************************************/
+
+
 
 int main (void)
 {
 	TIMER0_CONFIGURATION Timer_configStr={OVERFLOW_MODE, DISABLE, 0,0, DISABLE_INTERRUPT};
 	Timer0_init(&Timer_configStr);
-	sei();
 	Timer0_start(P1024);
 
 	while(1)
 	{
 		while(!Timer0_checkFlag());
+		Timer0_clearFlag();
 		counter++;
-		//_delay_ms(500);
-		if (counter ==50)
+
+		if (counter ==61)
 		{
 		TOGGLE_BIT(PORTC,1);
 		counter =0 ;
 		}
 	}
 }
+
+
+
+
+
